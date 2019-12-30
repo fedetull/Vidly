e47fca715c451349f12f334a5626acf5a4ceeb63
Luis Federico Tull <fedetull22@gmail.com>
2019-12-30 03:00:51 -0400

    Primer view y controlador con layout terminado

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Index()
         {
-
-            return View();
+            var movie = new Movie() { Name = "Shrek! " };
+            return View(movie);
         }
     }
 }
